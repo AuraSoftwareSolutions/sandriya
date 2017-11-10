9bc2a435dd0f2ac0a9d6f398eb887e71eef27337
fogez <410dheeraj@gmail.com>
2017-11-10 09:21:16 +0530

    contact form fix
    added validatons
    modified contactphp

--- contact.php
+++ contact.php
@@ -1,77 +1,84 @@
 <?php
-
-/**
- * configure here
+/*
+ *  CONFIGURE EVERYTHING HERE
  */
-$from = 'IT WORKER <it@domain.com>';
+
+// an email address that will be in the From field of the email.
+$from = 'Utopia@domain.com';
+
+// an email address that will receive the email with the output of the form
 $sendTo = 'projecttiltofficial@gmail.com';
+
+// subject of the email
 $subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
-$htmlHeader = '';
-$htmlFooter = '';
-$okMessage = 'Contact form succesfully submitted. Thank you, I will get back to you soon!';
 
-$htmlContent = '<h1>New message from contact form</h1>';
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
 
-/* DO NOT EDIT BELOW */
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
-/* use classes */
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
 
-use Nette\Mail\Message,
-    Nette\Mail\SendmailMailer;
+/*
+ *  LET'S DO THE SENDING
+ */
 
-/* require framework */
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
 
-require 'php/Nette/nette.phar';
+try
+{
 
-/* configure neccessary */
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+            
+    $emailText = "You have a new message from your contact form\n=============================\n";
 
-$configurator = new Nette\Configurator;
-$configurator->setTempDirectory(__DIR__ . '/php/temp');
-$container = $configurator->createContainer();
-
-/* get post */
-
-$httpRequest = $container->getService('httpRequest');
-$httpResponse = $container->getService('httpResponse');
-
-$post = $httpRequest->getPost();
-
-if ($httpRequest->isAjax()) {
-    /* compose htmlContent */
-
-    $htmlContent .= '<table>';
-    foreach ($post as $key => $value) {
-
-	if (isset($fields[$key])) {
-	    $htmlContent .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
-	}
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
     }
-    $htmlContent .= '</table>';
 
-    /* compose html body */
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    // Send email
+    $retval = mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
-    $htmlBody = $htmlHeader . $htmlContent . $htmlFooter;
+    if(isset($retval))//change
+    {
+        $responseArray = array('type' => 'success', 'message' => $okMessage);
+    }
+    else
+    {
+        $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    }
 
-    /* send email */
-
-    $mail = new Message;
-    $mail->setFrom($from)
-	    ->addTo($sendTo)
-	    ->setSubject($subject)
-	    ->setHtmlBody($htmlBody, FALSE);
-
-    $mailer = new SendmailMailer;
-    $mailer->send($mail);
-
-
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-
-    $httpResponse->setCode(200);
-    $response = new \Nette\Application\Responses\JsonResponse($responseArray);
-    $response->send($httpRequest, $httpResponse);
+    
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
 
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
 
+    header('Content-Type: application/json');
 
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
